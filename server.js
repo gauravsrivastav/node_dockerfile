d250095751a0d526ec15c1659fad6c624762a21b
omprakash <omprakash98527@gmail.com>
2017-05-16 14:28:08 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 4040;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello jenkins-docker-test\n');
+  res.send('Hello DevOps-jenkins-docker-test\n');
 });
 
 app.listen(PORT);
