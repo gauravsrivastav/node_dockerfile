2f7be22b9a00bfc5bdcb752c548fc3d36029cab0
gauravdevops <gaurav@farmguide.in>
2017-06-06 12:58:02 +0530

    changes in serevr.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 4041;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello, jenkins-github-integration\n');
+  res.send('Hello, JJenkins-github-integration\n');
 });
 
 app.listen(PORT);
